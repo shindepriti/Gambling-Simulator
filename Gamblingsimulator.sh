1844b7f76b3de8c21dc0c5008b950fbf647a00ca
shindepriti <pritishinde34@gmail.com>
2020-03-07 17:47:04 +0530

    ADDED:Function to calculate percentage gambelr Win Or loose Per Day

--- Gamblingsimulator.sh
+++ Gamblingsimulator.sh
@@ -3,14 +3,24 @@ echo "Wel-Come To Gambling Simulator "
 
 STAKE=100
 BET=1
+cash=$STAKE
+
+function calculatePercentage() {
+	MAX_CASH=$((STAKE+(50*STAKE/100)))
+	MIN_CASH=$((STAKE-(50*STAKE/100)))
+}
+calculatePercentage
 
 function checkLooseWin(){
-if [ $((RANDOM%2)) -eq 1 ]
-then
-	echo "WIN"
-else
-	echo "LOOSE"
-fi
+	while [[ $cash -ge $MIN_CASH && $cash -le $MAX_CASH ]]
+	do
+		if [ $((RANDOM%2)) -eq 1 ]
+		then
+			cash=$((cash+BET))
+		else
+			cash=$((cash-BET))
+		fi
+	done
 }
 checkLooseWin
 
